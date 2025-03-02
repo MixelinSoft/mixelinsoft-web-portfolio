f9641e5b6ec997b4dbe36355ccd716acc7f2777d
MixelinSoft <127510809+MixelinSoft@users.noreply.github.com>
2025-03-02 20:52:48 +0200

    add scroll to top button, enhance offcanvas menu, and improve logo interaction

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import localization from './data-localization.js';
 const selectedLanguage = localStorage.getItem('language') || 'ua';
 // Elements
+const logo = document.querySelector('.logo');
 const scrollButton = document.querySelector('.scroll');
+const scrollToTopButton = document.querySelector('.scrollToTop');
 const aboutSection = document.querySelector('.section-about');
 const docButtons = document.querySelectorAll('.cv-doc-button');
 const typewritterTerminal = document.querySelector('.typewriter-terminal');
@@ -17,6 +19,16 @@ window.addEventListener('scroll', () => {
   let scrollY = window.scrollY;
   scrollButton.style.transform = `translate(-50%, -${scrollY * 1.5}px)`;
   scrollButton.style.opacity = 1 - scrollY / 200;
+});
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollToTopButton.style.opacity = '0.75';
+    scrollToTopButton.style.visibility = 'visible';
+  } else {
+    scrollToTopButton.style.opacity = '0';
+    scrollToTopButton.style.visibility = 'hidden';
+  }
 });
 
 scrollButton.addEventListener('click', () => {
@@ -50,4 +62,11 @@ docButtons.forEach((button) => {
     const linkBlock = button.querySelector('.cv-links');
     linkBlock.classList.add('active');
   });
+});
+
+scrollToTopButton.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+logo.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
